ee2b1073de102cf4961ad3b400224f59d4326747
Ana Rubio <Ana.Rubio@uclm.es>
2024-11-18 10:59:24 +0100

    icestorm: print tmanager proxy

--- icestorm/publisher.py
+++ icestorm/publisher.py
@@ -15,7 +15,7 @@ class Publisher(Ice.Application):
             print("property {} not set".format(key))
             return None
 
-        print("Using IceStorm in: '%s'" % key)
+        print("Using IceStorm in: '%s'" % proxy)
         return IceStorm.TopicManagerPrx.checkedCast(proxy)
 
     def run(self, argv):
