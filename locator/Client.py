da48c44daf7da3ccbfca447f844f14799d3afb34
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2025-11-26 20:42:19 +0100

    update locator ex

--- locator/Client.py
+++ locator/Client.py
@@ -6,17 +6,19 @@ Ice.loadSlice('Printer.ice')
 import Example
 
 
-class Client(Ice.Application):
-    def run(self, argv):
-        proxy = self.communicator().stringToProxy(argv[1])
-        printer = Example.PrinterPrx.checkedCast(proxy)
+def main(ic):
+    proxy = ic.stringToProxy(sys.argv[2])
+    printer = Example.PrinterPrx.checkedCast(proxy)
 
-        if not printer:
-            raise RuntimeError('Invalid proxy')
+    if not printer:
+        raise RuntimeError('Invalid proxy')
 
-        printer.write('Hello World!')
-
-        return 0
+    printer.write('Hello World!')
 
 
-sys.exit(Client().main(sys.argv))
+if __name__ == "__main__":
+    try:
+        with Ice.initialize(sys.argv[1]) as communicator:
+            main(communicator)
+    except KeyboardInterrupt:
+        pass
